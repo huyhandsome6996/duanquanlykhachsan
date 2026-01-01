93d20c1357465af3458787af6bac161f47b94797
huyhandsome6996 <iamdoinb6996@gmail.com>
2026-01-01 20:02:31 +0700

    task5: thiet lap cau truc templates va static

--- config/settings.py
+++ config/settings.py
@@ -58,10 +58,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -117,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = 'static/'
