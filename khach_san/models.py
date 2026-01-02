f36cc994ada2eeb22fae9a3ea44dd0d81aa9b6c7
vietxuan2886 <vietxuan2886@gmail.com>
2026-01-02 17:21:14 +0700

    task 2 : tao model phong ( thiet ke model phong , cap nhat trang thai cua phong )

--- khach_san/models.py
+++ khach_san/models.py
@@ -7,3 +7,21 @@ class LoaiPhong(models.Model):
     )
     def __str__(self):
         return self.ten_loai
+
+class Phong(models.Model):
+    TRANG_THAI_CHOICES = [
+        ('trong', 'Trống'),
+        ('dang_thue', 'Đang thuê'),
+        ('dang_bao_tri', 'Đang bảo trì'),
+    ]
+
+    ma_phong = models.CharField(max_length=10, unique=True)
+    loai_phong = models.ForeignKey(LoaiPhong, on_delete=models.PROTECT)
+    trang_thai = models.CharField(
+        max_length=20,
+        choices=TRANG_THAI_CHOICES,
+        default='trong'
+    )
+
+    def __str__(self):
+        return f"Phòng {self.ma_phong}"
