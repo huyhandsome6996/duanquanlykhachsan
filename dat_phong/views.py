42997b0042c66fa88a32d7488b3a912716f80c21
buihuykhang2006 <buihuykhang2006@gmail.com>
2026-01-02 19:13:39 +0700

    task 7 hien thi danh sach phong (theo trang thai va thoi gian)

--- dat_phong/views.py
+++ dat_phong/views.py
@@ -15,3 +15,10 @@ from django.urls import reverse
 @staff_member_required
 def tao_dat_phong(request):
     pass
+
+# us-04: task: Hiển thị danh sách đặt phòng theo trạng thái và thời gian
+@staff_member_required
+def danh_sach_dat_phong(request):
+    pass
+
+
