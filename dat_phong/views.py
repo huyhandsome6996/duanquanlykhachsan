c7b96f221240e1aaafe375ed9dcdae3b0a29fd4e
huyhandsome6996 <iamdoinb6996@gmail.com>
2026-01-02 20:59:27 +0700

    python manage.py makemigrations,python manage.py migrate model

--- dat_phong/views.py
+++ dat_phong/views.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from datetime import datetime
 
 from khach_san.models import Phong
-from .models import DatPhong, SuDungDichVu, DichVu, LichHen
-from hoa_don.models import HoaDon
+from .models import DatPhong, SuDungDichVu, DichVu #,LichHen
+# from hoa_don.models import HoaDon
 
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required, user_passes_test
