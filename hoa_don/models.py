5bea79a73521e52ab6e0335f4a67f1de54bc77a5
NyenT <vbtnguyen2k6@gmail.com>
2026-01-03 18:25:10 +0700

    task 1

--- hoa_don/models.py
+++ hoa_don/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from dat_phong.models import DatPhong
 # Create your models here.
+class HoaDon(models.Model):
+    dat_phong = models.OneToOneField(
+        DatPhong,
+        on_delete=models.CASCADE,
+        related_name='hoa_don'
+    )
+
+    tien_phong = models.PositiveIntegerField(default=0)
+    tien_dich_vu = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Hoa don {self.id}"
