b40770325e4dbad6ad36c61404b49452501718fb
huyhandsome6996 <iamdoinb6996@gmail.com>
2026-01-03 18:13:55 +0700

    khung task 4,5. chua co model

--- hoa_don/views.py
+++ hoa_don/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+#from .models import HoaDon
+from dat_phong.models import DatPhong
+from django.shortcuts import redirect
 
-# Create your views here.
+#us-07: task: -Xem chi tiết hóa đơn (Hiển thị chi tiết hóa đơn của lượt đặt phòng.)
+def chi_tiet_hoa_don(request, dat_phong_id):
+    pass
+
+
+#us-07: task: -Xác nhận thanh toán hóa đơn (Cập nhật trạng thái hóa đơn sang đã thanh toán.)
+def xac_nhan_thanh_toan(request, dat_phong_id):
+    pass
