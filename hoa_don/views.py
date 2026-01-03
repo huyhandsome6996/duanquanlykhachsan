b61e54fa05323d181c4d861bf11ffba7cd0f6c62
NyenT <vbtnguyen2k6@gmail.com>
2026-01-03 18:35:14 +0700

    Task 4 Xem chi tiết hóa đơn (Hiển thị chi tiết hóa đơn của lượt đặt phòng.)

--- hoa_don/views.py
+++ hoa_don/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-#from .models import HoaDon
+from .models import HoaDon
 from dat_phong.models import DatPhong
 from django.shortcuts import redirect
 
 #us-07: task: -Xem chi tiết hóa đơn (Hiển thị chi tiết hóa đơn của lượt đặt phòng.)
 def chi_tiet_hoa_don(request, dat_phong_id):
-    pass
+    hoa_don = get_object_or_404(HoaDon, dat_phong_id=dat_phong_id)
+    return render(request, 'hoa_don/chi_tiet_hoa_don.html', {
+        'hoa_don': hoa_don
+    })
 
 
 #us-07: task: -Xác nhận thanh toán hóa đơn (Cập nhật trạng thái hóa đơn sang đã thanh toán.)
