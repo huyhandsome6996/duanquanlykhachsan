43321b92291e69c6ed4013866a42a01a559aac54
NyenT <vbtnguyen2k6@gmail.com>
2026-01-03 18:30:29 +0700

    task 2 Tạo model hóa đơn (Tổng tiền và trạng thái)

--- hoa_don/models.py
+++ hoa_don/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from dat_phong.models import DatPhong
 # Create your models here.
 class HoaDon(models.Model):
+    TRANG_THAI_CHOICES = [
+        ('chua_tt', 'Chưa thanh toán'),
+        ('da_tt', 'Đã thanh toán'),
+    ]
+
     dat_phong = models.OneToOneField(
         DatPhong,
         on_delete=models.CASCADE,
@@ -10,6 +15,13 @@ class HoaDon(models.Model):
 
     tien_phong = models.PositiveIntegerField(default=0)
     tien_dich_vu = models.PositiveIntegerField(default=0)
+    tong_tien = models.PositiveIntegerField(default=0)
+
+    trang_thai = models.CharField(
+        max_length=10,
+        choices=TRANG_THAI_CHOICES,
+        default='chua_tt'
+    )
 
     def __str__(self):
         return f"Hoa don {self.id}"
